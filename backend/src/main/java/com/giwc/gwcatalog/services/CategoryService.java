b36b11e86abffc997754a154e75e173cc8c32f90
Gabriel <wenck13@gmail.com>
2023-10-05 17:54:57 -0300

    Find category by id, exception handling

--- backend/src/main/java/com/giwc/gwcatalog/services/CategoryService.java
+++ backend/src/main/java/com/giwc/gwcatalog/services/CategoryService.java
@@ -3,11 +3,13 @@ package com.giwc.gwcatalog.services;
 import com.giwc.gwcatalog.dto.CategoryDTO;
 import com.giwc.gwcatalog.entities.Category;
 import com.giwc.gwcatalog.repositories.CategoryRepository;
+import com.giwc.gwcatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,10 +20,14 @@ public class CategoryService {
 
     @Transactional(readOnly = true)
     public List<CategoryDTO> findAll() {
-
         List<Category> list = repository.findAll();
-
         return list.stream().map(CategoryDTO::new).toList();
+    }
 
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
     }
 }
