2900551f662fbe781093ecfd44608d8e5c74d9fe
Gabriel <wenck13@gmail.com>
2023-10-05 16:27:49 -0300

    Database seeding

--- backend/src/main/java/com/giwc/gwcatalog/services/CategoryService.java
+++ backend/src/main/java/com/giwc/gwcatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.giwc.gwcatalog.entities.Category;
 import com.giwc.gwcatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,8 +14,8 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
-
         return repository.findAll();
     }
 }
