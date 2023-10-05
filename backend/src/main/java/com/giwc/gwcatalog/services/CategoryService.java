8a8ac76a9aee24a00300d5e25426bb03495066b8
Gabriel <wenck13@gmail.com>
2023-10-05 18:26:13 -0300

    Update Category

--- backend/src/main/java/com/giwc/gwcatalog/services/CategoryService.java
+++ backend/src/main/java/com/giwc/gwcatalog/services/CategoryService.java
@@ -3,14 +3,14 @@ package com.giwc.gwcatalog.services;
 import com.giwc.gwcatalog.dto.CategoryDTO;
 import com.giwc.gwcatalog.entities.Category;
 import com.giwc.gwcatalog.repositories.CategoryRepository;
-import com.giwc.gwcatalog.services.exceptions.EntityNotFoundException;
+import com.giwc.gwcatalog.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -27,7 +27,7 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
         Optional<Category> obj = repository.findById(id);
-        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
         return new CategoryDTO(entity);
     }
 
@@ -37,5 +37,17 @@ public class CategoryService {
         entity.setName(dto.getName());
         entity = repository.save(entity);
         return new CategoryDTO(entity);
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        try {
+            Category entity = repository.getReferenceById(id);
+            entity.setName(dto.getName());
+            entity = repository.save(entity);
+            return new CategoryDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found" + id);
+        }
     }
 }
