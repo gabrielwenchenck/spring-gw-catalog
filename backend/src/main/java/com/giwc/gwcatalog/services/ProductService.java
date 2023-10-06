59b5a549f0401d1826dccdd6509d3607b9683b8c
Gabriel <wenck13@gmail.com>
2023-10-05 21:07:11 -0300

    Product Insert & Update & Delete

--- backend/src/main/java/com/giwc/gwcatalog/services/ProductService.java
+++ backend/src/main/java/com/giwc/gwcatalog/services/ProductService.java
@@ -1,7 +1,10 @@
 package com.giwc.gwcatalog.services;
 
+import com.giwc.gwcatalog.dto.CategoryDTO;
 import com.giwc.gwcatalog.dto.ProductDTO;
+import com.giwc.gwcatalog.entities.Category;
 import com.giwc.gwcatalog.entities.Product;
+import com.giwc.gwcatalog.repositories.CategoryRepository;
 import com.giwc.gwcatalog.repositories.ProductRepository;
 import com.giwc.gwcatalog.services.exceptions.DatabaseException;
 import com.giwc.gwcatalog.services.exceptions.ResourceNotFoundException;
@@ -21,6 +24,8 @@ public class ProductService {
 
     @Autowired
     private ProductRepository repository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
     public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
@@ -38,16 +43,17 @@ public class ProductService {
     @Transactional
     public ProductDTO insert(ProductDTO dto) {
         Product entity = new Product();
-//        entity.setName(dto.getName());
+        copyDtoToEntity(dto, entity);
         entity = repository.save(entity);
         return new ProductDTO(entity);
     }
+
 
     @Transactional
     public ProductDTO update(Long id, ProductDTO dto) {
         try {
             Product entity = repository.getReferenceById(id);
-//            entity.setName(dto.getName());
+            copyDtoToEntity(dto, entity);
             entity = repository.save(entity);
             return new ProductDTO(entity);
         } catch (EntityNotFoundException e) {
@@ -63,6 +69,21 @@ public class ProductService {
             throw new ResourceNotFoundException("Id not found" + id);
         } catch (DataIntegrityViolationException e) {
             throw new DatabaseException("Integrity violation");
+        }
+    }
+
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+        entity.setDate(dto.getDate());
+
+        entity.getCategories().clear();
+        for (CategoryDTO catDto :
+                dto.getCategories()) {
+            Category category = categoryRepository.getReferenceById(catDto.getId());
+            entity.getCategories().add(category);
         }
     }
 }
